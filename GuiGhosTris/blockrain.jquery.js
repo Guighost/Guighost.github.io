ff3d12484456c108ca642270cb883f9cb92a183f
Duane E Burnett <dburnett3d@gmail.com>
2018-01-01 23:16:43 -0500

    GuiGhosTris styling and images

--- GuiGhosTris/blockrain.jquery.js
+++ GuiGhosTris/blockrain.jquery.js
@@ -20,16 +20,16 @@
       autoplay: false, // Let a bot play the game
       autoplayRestart: true, // Restart the game automatically once a bot loses
       showFieldOnStart: true, // Show a bunch of random blocks on the start screen (it looks nice)
-      theme: null, // The theme name or a theme object
-      blockWidth: 12, // How many blocks wide the field is (The standard is 10 blocks)
+      theme: 'candy', // The theme name or a theme object
+      //blockWidth: 10, // How many blocks wide the field is (The standard is 10 blocks)
       autoBlockWidth: true, // The blockWidth is dinamically calculated based on the autoBlockSize. Disabled blockWidth. Useful for responsive backgrounds
-      autoBlockSize: 30, // The max size of a block for autowidth mode
+      autoBlockSize: 24, // The max size of a block for autowidth mode
       difficulty: 'normal', // Difficulty (normal|nice|evil).
-      speed: 20, // The speed of the game. The higher, the faster the pieces go.
+      speed: 15, // The speed of the game. The higher, the faster the pieces go.
       asdwKeys: true, // Enable ASDW keys
 
       // Copy
-      playText: 'Play GuiGhosTris',
+      playText: 'Play Blockrain',
       playButtonText: 'Play',
       gameOverText: 'Game Over',
       restartButtonText: 'Play Again',
