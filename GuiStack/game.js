62cfcdf01fdc194ecc9e500453f787c0509f27d3
Duane E Burnett <dburnett3d@gmail.com>
2018-01-16 16:19:04 -0500

    Update game.js

--- GuiStack/game.js
+++ GuiStack/game.js
@@ -4,7 +4,7 @@ var levelScore = 0;
 var LEVEL = 1;
 var gameOptions = {
     timeLimit: 60,
-    gravity: 1500,
+    gravity: 3500,
     crateSpeed: 1000,
     crateHorizontalRange: 540,
     fallingHeight: 700,
